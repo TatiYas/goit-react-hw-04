d6e29460154975de0eccca0b956df17ff047937f
TatiYas <iasinovska_lawyer@ukr.net>
2024-07-28 19:09:26 +0200

    Update_15

--- src/components/images-api.js
+++ src/components/images-api.js
@@ -8,7 +8,7 @@ export const getImages = async (question, currentPage) => {
     params: {
       query: question,
       page: currentPage,
-      per_page: 12,
+      per_page: 11,
       client_id: 'Rh6Xv5HI-zfS2bHmKByQrvtE6pCTL4etJM6aiD5yaxA',
     },
   });

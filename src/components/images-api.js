447a886c8915720af2c8186307b75a7774564f18
TatiYas <iasinovska_lawyer@ukr.net>
2024-07-28 18:13:37 +0200

    Update_10

--- src/components/images-api.js
+++ src/components/images-api.js
@@ -1,15 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://api.unsplash.com/';
+axios.defaults.baseURL = "https://api.unsplash.com";
+const ACCESS_KEY = 'Rh6Xv5HI-zfS2bHmKByQrvtE6pCTL4etJM6aiD5yaxA';
 
-export const getImages = async (question, currentPage) => {
-  const response = await axios.get('/search/photos', {
-    params: {
-      query: question,
-      page: currentPage,
-      per_page: 12,
-      client_id: 'Rh6Xv5HI-zfS2bHmKByQrvtE6pCTL4etJM6aiD5yaxA',
-    },
-  });
-  return response.data;
-};
+async function fetchImages(searchQuery, page) {
+    const response = await axios.get('/search/images', {
+        params: {
+            client_id: ACCESS_KEY,
+            query: searchQuery,
+            page,
+        },
+    });
+    return response.data.results;
+}
+export default fetchImages;

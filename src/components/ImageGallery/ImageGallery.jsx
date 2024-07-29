a4edbba7b7566d6a3db60a9caefa9cf6ddca9a9f
TatiYas <iasinovska_lawyer@ukr.net>
2024-07-29 14:33:44 +0200

    Update_26

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,14 +4,11 @@ import s from './ImageGallery.module.css';
 export default function ImageGallery({ items, onImageClick }) {
   return (
     <ul className={s.gallery}>
-      {items.map(item => (
+      {items.map((item) => (
         <li key={item.id}>
           <ImageCard
-            imgLink={item.urls.small}
-            imgDescr={item.description}
-            onImageClick={onImageClick}
-            bigUrl={item.urls.regular}
-          />
+          items= {item} openModal={onImageClick}/>
+           
         </li>
       ))}
     </ul>

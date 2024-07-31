359505b757891733c36cd714b2b17c36837f206d
TatiYas <iasinovska_lawyer@ukr.net>
2024-07-31 18:46:21 +0200

    Update

--- src/components/images-api.js
+++ src/components/images-api.js
@@ -3,16 +3,23 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.unsplash.com/';
 
-export const getImages = async ( query, currentPage) => {
-  const response = await axios.get('/search/photos', {
+export const getImages = async ( query, currentPage) => { 
+  try{
+  const response = await axios.get('/search/photos', 
+    {
     params: {
       query:  query,
       page: currentPage,
-      per_page: 12,
+      per_page: 6,
       client_id: 'Rh6Xv5HI-zfS2bHmKByQrvtE6pCTL4etJM6aiD5yaxA',
     },
   });
   return response.data;
+} 
+catch (error) {
+  console.error("Error fetching images:", error);
+  throw error;
+}
 };
 
 
